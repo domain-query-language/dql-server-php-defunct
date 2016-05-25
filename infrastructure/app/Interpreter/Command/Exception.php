1e6836f9569c868b67125b23b03a291299f2765e
Barry O Sullivan <barry@tercet.io>
2016-05-25 22:23:43 +0100

    Added a command factory

---
+++ infrastructure/app/Interpreter/Command/Exception.php
@@ -0,0 +1,4 @@
+<?php namespace Infrastructure\App\Interpreter\Command;
+
+class Exception extends \Exception {} 
+
---
+++ infrastructure/app/Interpreter/Command/Interpreter.php
@@ -0,0 +1,9 @@
+<?php namespace Infrastructure\App\Interpreter\Command;
+
+use Infrastructure\App\Interpreter\ValueObject\Composite;
+
+class Interpreter extends Composite\Interpreter
+{    
+    
+}
+
