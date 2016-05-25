3024b4ee44ecf01a45aa4d26d99eaa42417b0513
Barry O Sullivan <barry@tercet.io>
2016-05-25 18:35:17 +0100

    Added entity AST and interpreter.

---
+++ infrastructure/app/Interpreter/Entity/Exception.php
@@ -0,0 +1,4 @@
+<?php namespace Infrastructure\App\Interpreter\Entity;
+
+class Exception extends \Exception {} 
+
---
+++ infrastructure/app/Interpreter/Entity/Interpreter.php
@@ -0,0 +1,11 @@
+<?php namespace Infrastructure\App\Interpreter\Entity;
+
+use Infrastructure\App\Interpreter\ValueObject\Composite;
+
+class Interpreter extends Composite\Interpreter
+{    
+    
+}
+
+
+
