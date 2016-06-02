0d7b20d1d187b83736234997171bcd6601d82269
Barry O Sullivan <barry@tercet.io>
2016-06-02 22:51:39 +0100

    Generalised EventStreams

--- app/EventStore/FullEventStream.php
+++ app/EventStore/FullEventStream.php
@@ -1,73 +1,9 @@
 <?php namespace App\EventStore;
 
-class FullEventStream implements \Iterator
+class FullEventStream extends AbstractEventStream
 {
-    private $chunk_size = 100;
-    private $streamed_count = 0;
-    
-    private $event_repo;
-    
-    private $events;
-       
-    public function __construct(EventRepository $event_repo)
+    protected function get_next_chunk($offset, $limit)
     {
-        $this->event_repo = $event_repo;
-        
-        $this->reset();
-        $this->fetch();
-    }
-    
-    protected function reset()
-    {
-        $this->streamed_count = 0;
-        $this->events = [];
-    }
-
-    protected function fetch()
-    {
-        $this->events = $this->get_next_chunk();
-    }
-        
-    private function get_next_chunk()
-    {
-        $offset = $this->streamed_count;
-        $limit = $this->chunk_size;
         return $this->event_repo->fetch_all($offset, $limit);
-    }
-    
-    protected function has_more_chunks()
-    {
-        return (count($this->events) == $this->chunk_size);
-    }
-    
-    public function current()
-    {
-        return current($this->events);
-    }
-    
-    public function next()
-    {
-        $event = next($this->events);
-        $this->streamed_count++;
-        if(! $event && $this->has_more_chunks()) {
-            $this->fetch();
-        }
-        
-        return $event;
-    }
-    
-    public function key()
-    {
-        return key($this->events);
-    }
-    
-    public function valid()
-    {
-        return current($this->events) !== false;
-    }
-    
-    public function rewind()
-    {
-     
     }
 }
