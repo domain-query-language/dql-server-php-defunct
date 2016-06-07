508ba4068a56b873145e94955264a0c02d726c4f
Barry O Sullivan <barry@tercet.io>
2016-06-07 13:34:05 +0100

    Added timeouts to the locks for SQLite. Removed inmemory lock as it is not used.

--- infrastructure/app/EventStore/PDO/EventStreamLocker.php
+++ infrastructure/app/EventStore/PDO/EventStreamLocker.php
@@ -2,31 +2,87 @@
 
 use App\EventStore\StreamID;
 use PDO;
+use App\EventStore\DateTimeGenerator;
+use DateTime;
 
 class EventStreamLocker implements \App\EventStore\EventStreamLocker
 {
     private $pdo;
+    private $datetime_generator;
     
-    public function __construct(PDO $pdo)
+    public function __construct(PDO $pdo, DateTimeGenerator $datetime_generator)
     {
         $this->pdo = $pdo;
+        $this->datetime_generator = $datetime_generator;
     }
     
     public function lock(StreamID $stream_id)
     {
+        $now = $this->datetime_generator->generate();
+        
         $key = $stream_id->domain_id.",".$stream_id->schema_id;
         
         $insert = "
             INSERT INTO event_stream_lock
-                (stream_id)
+                (stream_id, datetime)
             VALUES
-                (?)";
+                (?, ?)";
         
         try {
-            $this->pdo->prepare($insert)->execute([$key]);
+            $this->pdo->prepare($insert)->execute([$key, $now]);
         } catch (\PDOException $ex) {
-            throw new \App\EventStore\EventStreamLockerException();
+            if ($this->is_timed_out($key, $now)) {
+               $this->update_lock($key); 
+            } else {
+                throw new \App\EventStore\EventStreamLockerException();
+            }        
         }
+    }
+    
+    protected function is_timed_out($key, $now)
+    {
+        $query = "SELECT * FROM event_stream_lock WHERE stream_id = ?";
+        
+        $statement = $this->pdo->prepare($query);
+        $statement->execute([$key]);
+        
+        $row = $statement->fetchObject();
+        
+        $datetime = new DateTime($row->datetime);
+        
+        $diff = $this->diff_in_seconds(new DateTime($now), $datetime);
+        if ($diff < 0.5) {
+            return false;
+        }
+        return true;
+    }
+    
+    private function diff_in_seconds(DateTime $date1, DateTime $date2)
+    {
+        $diff = abs(strtotime($date1->format('d-m-Y H:i:s.u'))-strtotime($date2->format('d-m-Y H:i:s.u')));
+
+        $micro1 = $date1->format("u");
+        $micro2 = $date2->format("u");
+
+        $micro = abs($micro1 - $micro2);
+
+        $difference = $diff.".".$micro;
+
+        return (float)$difference;
+    }
+    
+    protected function update_lock($key) 
+    {
+        $update = "
+            UPDATE event_stream_lock
+               SET datetime = ?
+            WHERE 
+                stream_id = ?";
+        
+        $statement = $this->pdo->prepare($update);
+        
+        $now = $this->datetime_generator->generate();
+        $statement->execute([$now, $key]);
     }
     
     public function unlock(StreamID $stream_id)
