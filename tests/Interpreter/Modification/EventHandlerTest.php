cb6c60c071fa6e7bfe354e81238085eb2004245c
Barry O Sullivan <barry@tercet.io>
2016-06-15 22:56:29 +0100

    Can add to a list

--- tests/Interpreter/Modification/EventHandlerTest.php
+++ tests/Interpreter/Modification/EventHandlerTest.php
@@ -3,23 +3,35 @@
 use App\Interpreter\Modification\Interpreter;
 
 class EventHandlerTest extends \Test\Interpreter\TestCase
-{    
-    private $interpreter;
-    
-    public function setUp()
-    {
-        parent::setUp();
-        $ast = $this->ast_repo->event_handler_collection();
-        $this->interpreter = new Interpreter($ast->statements);
-    }
-    
+{
     public function test_adds_to_array()
     {
+        $ast = $this->ast_repo->event_handler_add_to_collection();
+        $interpreter = new Interpreter($ast->statements);
+        
         $root = (object)['list'=>[]];
         $event = (object)['value'=>'hello'];
         
         $expected = (object)['list'=>['hello']];
-        $actual = $this->interpreter->modify($root, $event);
+        $actual = $interpreter->modify($root, $event);
+        
+        $this->assertEquals($expected, $actual);
+    }
+    
+    public function test_removes_entity_from_array_by_id()
+    {
+        $ast = $this->ast_repo->event_handler_remove_from_collection();
+        $interpreter = new Interpreter($ast->statements);
+        
+        $id = '722a4a3f-96be-4ed3-b662-043cf8f8d4df';
+        $entity = (object)['id'=>$id, 'data'=>true];
+        $root = (object)['list'=>[$entity]];
+        
+        $entity->data = false;
+        $event = (object)['entity_id'=>$id];
+        
+        $expected = (object)['list'=>[]];
+        $actual = $interpreter->modify($root, $event);
         
         $this->assertEquals($expected, $actual);
     }
