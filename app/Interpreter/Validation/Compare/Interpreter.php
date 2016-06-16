14b3bf7dd1c47906f5970581e0dc4aa1b13850cb
Barry O Sullivan <barry@tercet.io>
2016-06-16 16:42:39 +0100

    Refactor of compare interpreter

--- app/Interpreter/Validation/Compare/Interpreter.php
+++ app/Interpreter/Validation/Compare/Interpreter.php
@@ -12,44 +12,55 @@ class Interpreter
         $this->comparator = $comparator;
         $this->value = $value;
     }
-    
-    public function check($value, $arguments=[])
+      
+    public function check($input, $arguments=null)
     {
-        if ($this->field != 'value') {
-            $field = $this->field;
-            $value = $value->$field;
-        }
+        $value = $this->get_value($input);
+        $compare_to_value = $this->get_compare_to_value($arguments);
         if ($this->comparator == "="){
-            return $value == $this->get_value($arguments);
+            return $value == $compare_to_value;
         }
         if ($this->comparator == "!="){
-            return $value != $this->get_value($arguments);
+            return $value != $compare_to_value;
         }
         if ($this->comparator == ">"){
-            return $value > $this->get_value($arguments);
+            return $value > $compare_to_value;
         }
         if ($this->comparator == "exists_in"){    
-            $argument = $this->get_value($arguments);
-            $list = array_filter($value, function($element) use ($argument){
-                if (isset($element->id)) {
-                    return ($element->id == $argument);
-                } else {
-                    return ($element == $argument);
-                }
-            });
-            return count($list) > 0;
+            return $this->is_in_list($value, $compare_to_value);
         }
 
         throw new Exception("Unknown comparator $this->comparator");
     }
     
-    private function get_value($arguments)
+    private function get_value($value)
+    {
+        if ($this->field != 'value') {
+            $field = $this->field;
+            return $value->$field;
+        }
+        return $value;
+    }
+    
+    private function get_compare_to_value($arguments)
     {
         if (isset($this->value->literal)) {
             return $this->value->literal;
         }
         $property = $this->value->property[0];
         return $arguments->$property;
+    }
+    
+    private function is_in_list($list, $value) 
+    {
+        $matches_list = array_filter($list, function($element) use ($value){
+            if (isset($element->id)) {
+                return ($element->id == $value);
+            } else {
+                return ($element == $value);
+            }
+        });
+        return count($matches_list) > 0;
     }
 }
 
