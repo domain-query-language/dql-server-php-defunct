25bbb3ffa4fd25c1989ff83c263e688032201b37
Barry O Sullivan <barry@tercet.io>
2016-06-18 00:09:28 +0100

    Added translator for commands between interpreter and commandstore

--- tests/Interpreter/CommandStoreTest.php
+++ tests/Interpreter/CommandStoreTest.php
@@ -35,11 +35,11 @@ class CommandStoreTest extends TestCase
         ];
     }
     
-    public function test_translates_event_before_storing_it()
+    public function test_translates_command_before_storing_it()
     {
         $command = $this->command;
         $this->builder->set_aggregate_id($command->domain->aggregate_id)
-                    ->set_schema_event_id($command->schema->id)
+                    ->set_schema_command_id($command->schema->id)
                     ->set_schema_aggregate_id($command->schema->aggregate_id)
                     ->set_payload($command->domain->payload);
         
@@ -49,7 +49,7 @@ class CommandStoreTest extends TestCase
                  ->method('store')
                  ->with($this->equalTo([$transformed]));
         
-        $this->store->store([$this->interpreter]);
+        $this->store->store([$this->command]);
     }
         
     public function test_fetch()
@@ -57,7 +57,7 @@ class CommandStoreTest extends TestCase
         $stream = ['command'];
         
         $this->infrastructure_store->expects($this->once())
-                 ->method('fetch_all')
+                 ->method('all')
                 ->willReturn($stream);
         
         $this->assertEquals(
